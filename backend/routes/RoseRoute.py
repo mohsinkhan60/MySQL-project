ba02659fa56fad7d21cc7eaa5f31f05887929d3d
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-05-15 20:33:37 +0500

    Add utility functions, type aliases, and validation classes to marshmallow
    - Introduced new utility functions in `utils.py` for handling various data types and operations, including timestamp conversions and value extraction from dictionaries.
    - Added type aliases in `types.py` to improve type hinting and readability across the marshmallow library.
    - Implemented a comprehensive set of validation classes in `validate.py` for common data validation scenarios, including URL, email, range, length, and custom validators.
    - Enhanced the structure and organization of the marshmallow library to support better validation and utility functionalities.

--- backend/routes/RoseRoute.py
+++ backend/routes/RoseRoute.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+from validations.RoseValidation import rose_schema
 
 rose_route = Blueprint("RoseRoute", __name__)
 
-@rose_route.route("/rose")
+@rose_route.route("/add-rose", methods=["GET", "POST"])
 def rose():
    return "This is Mohsin khan"
---
+++ backend/validations/RoseValidation.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields
+# add rose
+
+class RoseSchema(Schema):
+   title = fields.String(required=True, error_messages={"required": "Rose Title is required."})
+   desc = fields.String(required=True, validate=fields.Length(min=2, max=100), error_messages={
+      "required": "Rose desc is required."
+      })
+   image = fields.String(required=True, error_messages={"required": "Rose image is required."})
+
+# update rose
+
+# instance
+rose_schema = RoseSchema()
