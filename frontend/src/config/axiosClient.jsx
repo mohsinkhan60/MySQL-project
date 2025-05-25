7d6c2127f3305b6483279a00668c970e9d11d948
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-05-25 15:47:27 +0500

    fix: correct comment formatting for baseURL in axiosClient configuration

--- frontend/src/config/axiosClient.jsx
+++ frontend/src/config/axiosClient.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export const AxiosClient = axios.create({
-    baseURL: 'http://localhost:5000/api/v1/', // Replace with your API endpoint
-   //  baseURL: '/api/v1/', // Replace with your API endpoint
+    // baseURL: 'http://localhost:5000/api/v1/', // Replace with your API endpoint
+    baseURL: '/api/v1/', // Replace with your API endpoint
 })
